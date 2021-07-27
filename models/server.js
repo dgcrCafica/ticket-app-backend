6d129f8185a74cba62c35d180777885d889f42cd
David Geovanny <gcarrillo@cafica.com.mx>
2021-07-27 13:08:18 -0600

    Primer commit

---
+++ models/server.js
@@ -0,0 +1,43 @@
+// Servidor express
+const express  = require('express');
+const http     = require('http');
+const socketIO = require('socket.io');
+const path     = require('path');
+const Sockets  = require('./sockets');
+
+class Server {
+  constructor() {
+    this.app = express();
+    this.port = process.env.PORT;
+
+    // http server
+    this.server = http.createServer( this.app );
+
+    // Configuración de socket
+    this.io = socketIO( this.server, { /** Configuraciones */ } );
+  }
+
+  middlewares() {
+    // Configurar el directorio público
+    this.app.use( express.static( path.resolve( __dirname, '../public' ) ) );
+  }
+
+  configSockets() {
+    new Sockets( this.io );
+  }
+
+  execute() {
+    // Inicializar middlewares
+    this.middlewares();
+
+    // Inicializar sockets
+    this.configSockets();
+
+    // Inicializar server
+    this.server.listen(this.port, () => {
+      console.log('Server corriendo en el puerto: ', this.port);
+    });
+  }
+}
+
+module.exports = Server;
